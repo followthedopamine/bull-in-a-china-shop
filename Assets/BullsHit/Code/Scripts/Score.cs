4fee35c8ac3e0d0de476b60372aaecb00d427f3d
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-11-06 09:48:56 +1300

    Bug: Fixed a bug where the player could score more than once on the same object

--- Assets/BullsHit/Code/Scripts/Score.cs
+++ Assets/BullsHit/Code/Scripts/Score.cs
@@ -12,9 +12,15 @@ public class Score : MonoBehaviour {
 
   void OnCollisionEnter(Collision hit) {
     if (hit.gameObject.tag == "Destructible") {
-      int objectValue = hit.gameObject.GetComponent<ScoreValue>().value;
-      score += objectValue;
+      ScoreValue scoreValue;
+      // Since OpenFragments inherit the Destructible tag from their parent we will get an error on collision with them if we try to access the ScoreValue script
+      if (scoreValue = hit.gameObject.GetComponent<ScoreValue>()) {
+        int objectValue = scoreValue.value;
+        scoreValue.setValue(0);
+        score += objectValue;
+      }
       scoreText.text = "Score: " + score;
     }
   }
+
 }
--- Assets/BullsHit/Code/Scripts/ScoreValue.cs
+++ Assets/BullsHit/Code/Scripts/ScoreValue.cs
@@ -4,4 +4,8 @@ using UnityEngine;
 
 public class ScoreValue : MonoBehaviour {
   public int value = 10;
+
+  public void setValue(int score) {
+    value = score;
+  }
 }
